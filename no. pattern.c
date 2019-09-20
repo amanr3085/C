b5bfe4b6b57f834360a96baae3d9336a9dd75afe
amanr3085 <54076289+amanr3085@users.noreply.github.com>
2019-09-20 09:44:54 +0530

    Add files via upload

---
+++ no. pattern.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+int main() {
+
+    for(int i=0;i<5;i++)
+    {
+        for(int j=1;j<=5;j++)
+        {
+            printf("%d",j);
+            
+        
+        }
+        
+        printf("\n");
+    }
+}
