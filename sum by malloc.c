81d297a5039bd89dfe5f00ff5f2cb8b440396bed
amanr3085 <54076289+amanr3085@users.noreply.github.com>
2020-01-31 11:32:51 +0530

    Add files via upload

---
+++ sum by malloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<stlib.h>
+int main()
+{
+    int i,n,*ptr,sum=0;
+    printf("Enter no.=");
+    scanf("%d",n);
+    ptr=(int*)malloc(n*size(int));
+    //if memory not allocated
+    if(ptr==null);
+    {
+        printf("Error");
+        exit(0);
+    }
+    printf("Enter elements");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",ptr+i);
+        sum+=*(ptr+i);
+    }
+    printf("sum=%d",sum);
+    free(ptr);
+
+    return 0;
+}
