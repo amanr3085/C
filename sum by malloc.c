d091f4cae0afac4184ebb7485633685267427b74
amanr3085 <54076289+amanr3085@users.noreply.github.com>
2021-07-21 17:55:30 +0530

    Update and rename error sum by malloc.c to sum by malloc.c

--- sum by malloc.c
+++ sum by malloc.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
-#include<stlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 int main()
 {
     int i,n,*ptr,sum=0;
-    printf("Enter no.=");
+    printf("Enter number = ");
     scanf("%d",&n);
-    ptr=(int*)malloc(n*sizeof(int));
+    ptr=(int*) malloc (n*sizeof(int));
     //if memory not allocated
-    if(ptr==null);
+    if(ptr==NULL)
     {
         printf("Error");
         exit(0);
     }
-    printf("Enter elements");
+    printf("Enter elements : \n");
     for(i=0;i<n;i++)
     {
-        scanf("%d",&(ptr+i));
+        scanf("%d",ptr+i);
         sum+=*(ptr+i);
     }
     printf("sum=%d",sum);
